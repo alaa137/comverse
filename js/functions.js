54c8dbaf01e9728097612f91ab207d0d663f4460
ALAA <alaa137@gmail.com>
2015-05-01 16:46:42 +0300

    bug fixes

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,3 @@
-/**
- * Created by alaa1_000 on 27/4/2015.
- */
 
 var introduction_bg = document.querySelector(".introduction");
 var slideshow_icon_1 = document.querySelector(".slideshow_1");
@@ -8,34 +5,38 @@ var slideshow_icon_2 = document.querySelector(".slideshow_2");
 var slideshow_icon_3 = document.querySelector(".slideshow_3");
 var slideshow_icon_4 = document.querySelector(".slideshow_4");
 
-slideshow_icon_1.addEventListener("click",function(){
-   introduction_bg.style.backgroundImage = "url('img/slide-bg-complexity.jpg')";
-   slideshow_icon_1.style.backgroundColor = "gray";
-   slideshow_icon_2.style.backgroundColor = null;
-   slideshow_icon_3.style.backgroundColor = null;
-   slideshow_icon_4.style.backgroundColor = null;
-});
+window.onload = slideShow(1);
 
-slideshow_icon_2.addEventListener("click",function(){
-	introduction_bg.style.backgroundImage = "url('img/homepage_bulb.jpg')";
-   slideshow_icon_1.style.backgroundColor = null;
-   slideshow_icon_2.style.backgroundColor = "gray";
-   slideshow_icon_3.style.backgroundColor = null;
-   slideshow_icon_4.style.backgroundColor = null;
-});
+function slideShow(icon)
+{
+	slideshow_icon_1.style.backgroundColor = null;
+	slideshow_icon_2.style.backgroundColor = null;
+	slideshow_icon_3.style.backgroundColor = null;
+	slideshow_icon_4.style.backgroundColor = null;
+	
+	if(icon == 1)
+	{
+		slideshow_icon_1.style.backgroundColor = "gray";
+		introduction_bg.style.backgroundImage = "url('img/slide-bg-complexity.jpg')";
+	}
+	else if(icon == 2)
+	{
+		slideshow_icon_2.style.backgroundColor = "gray";
+		introduction_bg.style.backgroundImage = "url('img/homepage_bulb.jpg')";
+	}
+	else if(icon == 3)
+	{
+		slideshow_icon_3.style.backgroundColor = "gray";
+		introduction_bg.style.backgroundImage = "url('img/HP_TOP_Be_Ready.jpg')";
+	}
+	else if(icon == 4)
+	{
+		slideshow_icon_4.style.backgroundColor = "gray";
+		introduction_bg.style.backgroundImage = "url('img/hp_top_be_prosperous.jpg')";
+	}
+}
 
-slideshow_icon_3.addEventListener("click",function(){
-   introduction_bg.style.backgroundImage = "url('img/HP_TOP_Be_Ready.jpg')";
-   slideshow_icon_1.style.backgroundColor = null;
-   slideshow_icon_2.style.backgroundColor = null;
-   slideshow_icon_3.style.backgroundColor = "gray";
-   slideshow_icon_4.style.backgroundColor = null;
-});
-
-slideshow_icon_4.addEventListener("click",function(){
-   introduction_bg.style.backgroundImage = "url('img/hp_top_be_prosperous.jpg')";
-   slideshow_icon_1.style.backgroundColor = null;
-   slideshow_icon_2.style.backgroundColor = null;
-   slideshow_icon_3.style.backgroundColor = null;
-   slideshow_icon_4.style.backgroundColor = "gray";
-});
+slideshow_icon_1.addEventListener("click",function(){slideShow(1);});
+slideshow_icon_2.addEventListener("click",function(){slideShow(2);});
+slideshow_icon_3.addEventListener("click",function(){slideShow(3);});
+slideshow_icon_4.addEventListener("click",function(){slideShow(4);});
